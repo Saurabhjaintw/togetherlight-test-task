622f39df08467347704563c24b5099a586de93dc
Saurabhjaintw <saurabhjaintw@gmail.com>
2023-09-06 15:04:13 +0530

    added the custom input component

---
+++ src/components/inputComponent/styles.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+export const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 5,
+    width: '100%',
+  },
+  input: {
+    paddingHorizontal: 24,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 12,
+    paddingVertical: 15,
+    fontWeight: '500',
+    marginHorizontal: 16,
+  },
+  label: {
+    fontWeight: '500',
+    fontSize: 14,
+    marginHorizontal: 16,
+
+    marginVertical: 6,
+  },
+  error_text: {
+    marginTop: 5,
+    color: 'red',
+    fontSize: 14,
+    fontWeight: 500,
+    marginHorizontal: 20,
+  },
+});
