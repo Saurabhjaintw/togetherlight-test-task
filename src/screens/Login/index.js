64f6d319081cf9511a4c9cc8c95a7a718e35fc93
Saurabhjaintw <saurabhjaintw@gmail.com>
2023-09-07 11:34:04 +0530

    task 1 completed

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, View} from 'react-native';
 import {styles} from './styles';
+import InputComponent from '../../components/inputComponent';
+import ButtonComponent from '../../components/buttonComponent';
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handlePress = () => {
+    if (
+      userName?.toLowerCase() === 'admin' &&
+      password?.toLocaleLowerCase() === 'admin123'
+    ) {
+      Alert.alert('Login Successful', 'Go to Dashboard', [
+        {
+          text: 'Ok',
+          onPress: () => null,
+        },
+      ]);
+    } else {
+      Alert.alert('Invalid Credentials', 'User name or password invalid');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
-        <Text>Go to home page</Text>
-      </TouchableOpacity>
+      <View>
+        <InputComponent
+          name={userName}
+          setName={setUserName}
+          placeholder={'Enter Your Username'}
+          label={'Username'}
+        />
+      </View>
+      <View>
+        <InputComponent
+          name={password}
+          setName={setPassword}
+          placeholder={'Enter Your Password'}
+          label={'Password'}
+          isSecure={true}
+        />
+      </View>
+
+      <ButtonComponent
+        disabled={!(userName?.length && password?.length)}
+        title={'Submit'}
+        onPress={handlePress}
+      />
     </View>
   );
 };
